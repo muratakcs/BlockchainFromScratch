1613349d845f932071cbdb8bc6b2ff629e3873f4
Murat Ak <muratakcs@gmail.com>
2023-06-07 00:07:23 +0300

    Cleaned and commented TransactionOutput class

--- TransactionOutput.java
+++ TransactionOutput.java
@@ -6,10 +6,10 @@ import java.math.BigInteger;
 
 //We can think of transaction outputs as a check that can be turned into cash and spent later.
 class TransactionOutput {
-    public String id;
-    public PublicKey recipient;
-    public int value;
-    public String parentTransactionId;
+    public String id;           // if for referring
+    public PublicKey recipient; // whom this TXO belongs to
+    public int value;           // the value of the TXO
+    public String parentTransactionId; // The tx in which this TXO is created 
 
     public TransactionOutput(PublicKey recipient, int value, String parentTransactionId) {
         this.recipient = recipient;
@@ -31,17 +31,25 @@ class TransactionOutput {
         }
         return bytesToHex(hash); // This function converts byte array into a hexadecimal string.
     }
+
+    // Finds out whether a TXO belongs to a particular public key
+    // So checks whether the public key of the object (recipient)
+    // is equal to the incoming parameter (publicKey) or not
+    public boolean isMine(PublicKey publicKey) {
+        return (publicKey.equals(recipient));
+    }
     
+
+    
+    
+    // Static utility function to turn a byte array hash into a string 
     public static String bytesToHex(byte[] hash) {
-        BigInteger number = new BigInteger(1, hash);
-        StringBuilder hexString = new StringBuilder(number.toString(16));
+        BigInteger number = new BigInteger(1, hash);    // Take the hash as a big integer
+        StringBuilder hexString = new StringBuilder(number.toString(16)); // turn it into hex
+        // padding to 32 hex characters with leading zeros
         while (hexString.length() < 32) {
             hexString.insert(0, '0');
         }
         return hexString.toString();
-    }
-
-    public boolean isMine(PublicKey publicKey) {
-        return (publicKey.equals(recipient));
-    }
+    }    
 }
